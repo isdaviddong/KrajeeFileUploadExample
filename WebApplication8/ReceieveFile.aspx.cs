afc598e5923b6ba65ee68d5af17a8c65f516e2e1
David@SurfacePro4 <isdaviddong@gmail.com>
2017-02-02 16:16:50 +0800

    update upload max length 增加註解

--- WebApplication8/ReceieveFile.aspx.cs
+++ WebApplication8/ReceieveFile.aspx.cs
@@ -7,43 +7,57 @@ using System.Web.UI.WebControls;
 
 namespace WebApplication8
 {
+    /// <summary>
+    /// 回傳物件
+    /// </summary>
     public class customResponse
     {
+        /// <summary>
+        /// 我們只回傳一個URL
+        /// </summary>
         public string URL { get; set; }
     }
     public partial class ReceieveFile : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            //配合前端需要接收pure JSON response
             Response.Clear();
             Response.ContentType = "application/json";
-
+            //理論上，只會有1
             if (Request.Files.Count > 0)
             {
                 try
                 {
+                    //我們把上傳的檔案放到temp資料夾底下
                     string path = Server.MapPath("temp\\");
                     for (int i = 0; i < Request.Files.Count; i++)
                     {
+                        //為了上傳檔案不會重複，因此檔名一率加上guid
                         var guid = Guid.NewGuid();
                         var file = Request.Files[i];
                         path += guid + "-" + file.FileName;
+                        //存檔
                         file.SaveAs(path);
                         //done
                         var JSON = Newtonsoft.Json.JsonConvert.SerializeObject(
                             new customResponse { URL = "temp\\" + guid + "-" + file.FileName });
+                        //回應JSON
                         Response.Write(JSON);
                     }
                 }
                 catch (Exception ex)
                 {
+                    //如果有錯誤，回應exception JSON
                     string jsonError = Newtonsoft.Json.JsonConvert.SerializeObject(ex);
                     Response.Write(jsonError);
                 }
+                //stop response
                 Response.End();
             }
             else
             {
+                //如果別人亂入
                 string jsonError = Newtonsoft.Json.JsonConvert.SerializeObject(new Exception("No Any Files."));
                 Response.Write(jsonError);
                 Response.End();
